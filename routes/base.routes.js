70110c1a6789efa5ce7ce72e899ba21e4448e469
Barbara Diaz <barbarad91@gmail.com>
2021-02-11 16:27:53 +0100

    Redistribute endpoints

--- routes/base.routes.js
+++ routes/base.routes.js
@@ -1,7 +1,36 @@
 const express = require("express")
 const router = express.Router()
+const Place = require("../models/place.model")
 
 // Endpoints
 router.get("/", (req, res) => res.render("index"))
+
+router.get("/index", async (req, res, next) => {
+  const placeType =
+    req.query.type === "coffee-shops" ? "coffee shop" : "bookstore"
+  try {
+    const placesList = await Place.find({ type: placeType }).select("name")
+    res.render("list-index", { placesList })
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get("/new", (req, res) => {
+  const type = req.query.type
+  res.render("new-place", { type })
+})
+
+router.post("/new", async (req, res, next) => {
+  const placeType =
+    req.query.type === "coffee-shops" ? "coffee shop" : "bookstore"
+  const { name } = req.body
+  try {
+    Place.create({ name, type: placeType })
+    res.redirect("/")
+  } catch (err) {
+    next
+  }
+})
 
 module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,4 @@
 module.exports = (app) => {
   // Base URLS
   app.use("/", require("./base.routes.js"))
-  app.use("/coffee-shops", require("./coffee-shops.routes"))
-  app.use("/bookstores", require("./bookstores.routes"))
 }
